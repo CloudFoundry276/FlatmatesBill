8f79ee9292285e414716630c21095c82cbedd6c3
CloudFoundry276 <cloudfoundry276@gmail.com>
2022-01-29 02:12:17 +0530

    done

---
+++ flatmates_bill/main.py
@@ -0,0 +1,25 @@
+from flat import Bill, Flatmate
+from reports import PdfReport, FileSharer
+
+amount = float(input("Hey user, enter the bill amount: "))
+period = input("What is the bill period (ex. December 2020): ")
+
+name1 = input("Enter name of first flatmate: ")
+days1 = int(input(f"How many days did {name1} stay in the house during the bill period: "))
+
+name2 = input("Enter name of second flatmate: ")
+days2 = int(input(f"How many days did {name2} stay in the house during the bill period: "))
+
+the_bill = Bill(amount, period)
+flatmate1 = Flatmate(name1, days1)
+flatmate2 = Flatmate(name2, days2)
+
+print("Amount paid by {} is ${}".format(flatmate1.name, flatmate1.pays(the_bill, flatmate2)))
+print("Amount paid by {} is ${}".format(flatmate2.name, flatmate2.pays(the_bill, flatmate1)))
+
+pdf_report = PdfReport("{}.pdf".format(the_bill.period))
+pdf_report.generate(flatmate1, flatmate2, the_bill)
+
+
+file_sharer = FileSharer(filepath=pdf_report.filename)
+print(file_sharer.share())
--- main.py
+++ main.py
@@ -1,25 +1,56 @@
-from flat import Bill, Flatmate
-from reports import PdfReport, FileSharer
-
-amount = float(input("Hey user, enter the bill amount: "))
-period = input("What is the bill period (ex. December 2020): ")
-
-name1 = input("Enter name of first flatmate: ")
-days1 = int(input(f"How many days did {name1} stay in the house during the bill period: "))
-
-name2 = input("Enter name of second flatmate: ")
-days2 = int(input(f"How many days did {name2} stay in the house during the bill period: "))
-
-the_bill = Bill(amount, period)
-flatmate1 = Flatmate(name1, days1)
-flatmate2 = Flatmate(name2, days2)
-
-print("Amount paid by {} is ${}".format(flatmate1.name, flatmate1.pays(the_bill, flatmate2)))
-print("Amount paid by {} is ${}".format(flatmate2.name, flatmate2.pays(the_bill, flatmate1)))
-
-pdf_report = PdfReport("{}.pdf".format(the_bill.period))
-pdf_report.generate(flatmate1, flatmate2, the_bill)
+from flask.views import MethodView
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template, request
+from flatmates_bill import flat
 
 
-file_sharer = FileSharer(filepath=pdf_report.filename)
-print(file_sharer.share())
+app = Flask(__name__)
+
+
+class HomePage(MethodView):
+
+    def get(self):
+        return render_template('index.html')
+
+
+class BillFormPage(MethodView):
+
+    def get(self):
+        bill_form = BillForm()
+        return render_template('bill_form_page.html', billform=bill_form)
+
+
+class ResultsPage(MethodView):
+
+    def post(self):
+        billform = BillForm(request.form)
+
+        the_bill = flat.Bill(float(billform.amount.data), billform.period.data)
+        flatmate1 = flat.Flatmate(billform.name1.data, float(billform.days_in_house1.data))
+        flatmate2 = flat.Flatmate(billform.name2.data, float(billform.days_in_house2.data))
+
+        return render_template('results.html',
+                               name1=flatmate1.name,
+                               amount1=flatmate1.pays(the_bill, flatmate2),
+                               name2=flatmate2.name,
+                               amount2=flatmate2.pays(the_bill, flatmate1))
+
+
+class BillForm(Form):
+    amount = StringField("Bill Amount: ", default=100)
+    period = StringField("Bill Period: ", default="December 2020")
+
+    name1 = StringField("Name: ", default="John")
+    days_in_house1 = StringField("Days in the house: ", default=20)
+
+    name2 = StringField("Name: ", default="Mary")
+    days_in_house2 = StringField("Days in the house: ", default=12)
+
+    button = SubmitField("Calculate")
+
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill_form', view_func=BillFormPage.as_view('bill_form_page'))
+app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
+
+app.run(debug=True)
